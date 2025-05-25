2b39d3dbf4cd79976289c4953e304f2c61ba8cf3
مهدی عطایی <92110428+mahdiAtaee@users.noreply.github.com>
2025-05-25 15:23:29 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Banner from "@/components/home/Banner";
 import SmallBanner from "@/components/home/SmallBanner";
 import DiscountedProduct from "@/components/home/DiscountedProduct";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
     <ShopLayout title="فروشگاه">
